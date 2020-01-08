aa4dcfb942f9a397ea26c5084ed85f2d0de43f14
Ahmad Wilson <ahwilson712@gmail.com>
2020-01-08 12:14:33 -0500

    add ansible_managed logic to templating

--- test/integration/config/default_test.rb
+++ test/integration/config/default_test.rb
@@ -4,8 +4,6 @@ describe file('/etc/tmux.conf') do
   it { should exist }
   its('mode') { should cmp '0644' }
 
-  its('content') { should match("Managed by Ansible") }
-  its('content') { should match("ansible-role-tmux") }
   its('content') { should match("Add ctrl-A as secondary prefix") }
   its('content') { should match("set -g prefix2 C-a") }
 end
@@ -14,8 +12,6 @@ describe file('/home/kitchen/.tmux.conf') do
   it { should exist }
   its('mode') { should cmp '0644' }
 
-  its('content') { should match("Managed by Ansible") }
-  its('content') { should match("ansible-role-tmux") }
   its('content') { should match("Enable faster command sequences") }
   its('content') { should match("set -s escape-time 10") }
 end
